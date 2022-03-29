7e8949c727c96a42b74b32897f82d62d32144a6e
Victor Tran <vicr12345@gmail.com>
2022-03-29 21:35:04 +1100

    Fix extra install dirs

--- lib/cmake-scripts/cntp-extrainstalldirs.cmake
+++ lib/cmake-scripts/cntp-extrainstalldirs.cmake
@@ -1,15 +1,29 @@
 function(cntp_define_build_dirs)
+    find_path(QMAKE_PATH qmake6)
+    IF(${QMAKE_PATH} STREQUAL "QMAKE_PATH-NOTFOUND")
+        message(FATAL_ERROR "Couldn't call qmake. Ensure Qt 6 is installed correctly and qtpaths is located in your PATH.")
+    ENDIF()
+
+    set(QMAKE_PATH ${QMAKE_PATH}/qmake6)
+
     execute_process(
-            COMMAND qmake6 -query QT_INSTALL_PLUGINS
+            COMMAND ${QMAKE_PATH} -query QT_INSTALL_PREFIX
+            RESULT_VARIABLE QT_PREFIX_RESULT
+            OUTPUT_VARIABLE QT_PREFIX_DIR
+    )
+
+    string(STRIP "${QT_PREFIX_DIR}" QT_PREFIX_DIR)
+    file(TO_CMAKE_PATH "${QT_PREFIX_DIR}" QT_PREFIX_DIR)
+
+    execute_process(
+            COMMAND ${QMAKE_PATH} -query QT_INSTALL_PLUGINS
             RESULT_VARIABLE PLUGIN_INSTALLATION_DIR_RESULT
             OUTPUT_VARIABLE PLUGIN_INSTALLATION_DIR
     )
 
-    if (PLUGIN_INSTALLATION_DIR_RESULT EQUAL 0)
-        string(STRIP "${PLUGIN_INSTALLATION_DIR}" PLUGIN_INSTALLATION_DIR)
-        file(TO_CMAKE_PATH "${PLUGIN_INSTALLATION_DIR}" PLUGIN_INSTALLATION_DIR)
-        set(CNTP_INSTALL_PLUGINS ${PLUGIN_INSTALLATION_DIR} PARENT_SCOPE)
-    else()
-        message(FATAL_ERROR "Couldn't call qtpaths. Ensure Qt 6 is installed correctly and qtpaths is located in your PATH.")
-    endif()
+    string(STRIP "${PLUGIN_INSTALLATION_DIR}" PLUGIN_INSTALLATION_DIR)
+    file(TO_CMAKE_PATH "${PLUGIN_INSTALLATION_DIR}" PLUGIN_INSTALLATION_DIR)
+
+    file(RELATIVE_PATH PLUGIN_INSTALLATION_DIR "${QT_PREFIX_DIR}" "${PLUGIN_INSTALLATION_DIR}")
+    set(CNTP_INSTALL_PLUGINS ${CMAKE_INSTALL_PREFIX}/${PLUGIN_INSTALLATION_DIR} PARENT_SCOPE)
 endfunction()
