70c37260a05f70dfd79b00d0854c954ae80e571d
Victor Tran <vicr12345@gmail.com>
2022-04-05 22:29:28 +1000

    Ignore nonexistent library files

--- cntp-macdeploy/library.cpp
+++ cntp-macdeploy/library.cpp
@@ -86,6 +86,11 @@ void Library::lipo(QMap<QString, QString> toMerge, QString output) {
             libFile = libFileInfo.absoluteFilePath();
         }
 
+        if (!QFile::exists(libFile)) {
+            QTextStream(stderr) << libFile << " does not exist. The application bundle may not be complete.\n";
+            continue;
+        }
+
         QTemporaryFile temporary;
         temporary.open();
         QString tempFile = temporary.fileName();
