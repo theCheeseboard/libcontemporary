457373c75e0e0779e19938f4767785d818bfca20
Victor Tran <vicr12345@gmail.com>
2022-10-31 21:39:52 +1100

    Fix translations

--- lib/translations/tr_TR.ts
+++ lib/translations/tr_TR.ts
@@ -1,23 +1,3 @@
-/*
- *
- * libcontemporary - Common libraries
- * Copyright (C) 2022 Victor Tran
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="tr_TR" sourcelanguage="en_US">
