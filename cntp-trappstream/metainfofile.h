b2165beebfad01438ca0855fbc1414cd75e14b44
Victor Tran <vicr12345@gmail.com>
2023-02-24 23:21:50 +1100

    Add cntp-trappstream

---
+++ cntp-trappstream/metainfofile.h
@@ -0,0 +1,26 @@
+#ifndef METAINFOFILE_H
+#define METAINFOFILE_H
+
+#include <QIODevice>
+#include <QString>
+
+struct MetainfoFilePrivate;
+class MetainfoFile {
+    public:
+        MetainfoFile(QString file);
+        ~MetainfoFile();
+
+        void setOutput(QIODevice* device);
+
+        bool moveToNext();
+
+        QString trKey();
+        QString trContents();
+
+        void writeTranslation(QString language, QString contents);
+
+    private:
+        MetainfoFilePrivate* d;
+};
+
+#endif // METAINFOFILE_H
