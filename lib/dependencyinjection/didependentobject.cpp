b646b58f7d5a95178e0d76bcded889e97b009698
Victor Tran <vicr12345@gmail.com>
2023-07-24 09:36:26 +1000

    Remove constructor checks

--- lib/dependencyinjection/didependentobject.cpp
+++ lib/dependencyinjection/didependentobject.cpp
@@ -7,7 +7,6 @@
 
 struct DIDependentObjectPrivate {
         QMetaObject constructible;
-        QMetaMethod constructor;
         QStringList dependencies;
 
         tDIManager* diManager;
@@ -18,22 +17,6 @@ DIDependentObject::DIDependentObject(QMetaObject constructible, tDIManager* diMa
     d = new DIDependentObjectPrivate();
     d->constructible = constructible;
     d->diManager = diManager;
-
-    auto name = QString::fromUtf8(d->constructible.className());
-    auto constructors = d->constructible.constructorCount();
-
-    for (auto i = 0; i < d->constructible.constructorCount(); i++) {
-        auto constructor = d->constructible.constructor(i);
-        if (constructor.parameterCount() == 0) {
-            d->constructor = constructor;
-            break;
-        }
-    }
-
-    if (!d->constructor.isValid()) {
-        // The constructible has no valid constructor
-        tCritical("DIDependentObject") << "Class " << constructible.className() << " registered for dependency injection has no default constructor";
-    }
 }
 
 QSharedPointer<tDIBaseInterface> DIDependentObject::construct() {
