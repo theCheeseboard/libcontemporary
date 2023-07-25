ec678435b682b2324d19fb392eec95a61aaef17c
Victor Tran <vicr12345@gmail.com>
2023-07-25 19:41:05 +1000

    Automatically set Windows SDK version

--- lib/cmake-scripts/cntp-winappx.cmake
+++ lib/cmake-scripts/cntp-winappx.cmake
@@ -1,13 +1,16 @@
 include_guard()
 
 function(cntp_winappx target appxmanifest)
-    get_target_property(APPX_DISPLAY_NAME ${target} READABLE_NAME)
-    get_target_property(APPX_EXECUTABLE_NAME ${target} OUTPUT_NAME)
+    if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+        get_target_property(APPX_DISPLAY_NAME ${target} READABLE_NAME)
+        get_target_property(APPX_EXECUTABLE_NAME ${target} OUTPUT_NAME)
 
-    cntp_repoversion(REPOVERSION)
+        cntp_repoversion(REPOVERSION)
 
-    set(APPX_EXECUTABLE_NAME ${APPX_EXECUTABLE_NAME}.exe)
-    set(APPX_ARCH x64) # TODO: Update
-    set(APPX_VERSION ${REPOVERSION_MAJOR}.${REPOVERSION_MINOR}.${REPOVERSION_REVISION}.0)
-    configure_file(${appxmanifest} appxmanifest.xml)
+        set(APPX_EXECUTABLE_NAME ${APPX_EXECUTABLE_NAME}.exe)
+        set(APPX_ARCH x64) # TODO: Update
+        set(APPX_VERSION ${REPOVERSION_MAJOR}.${REPOVERSION_MINOR}.${REPOVERSION_REVISION}.0)
+        set(APPX_SDK_VERSION ${CMAKE_SYSTEM_VERSION})
+        configure_file(${appxmanifest} appxmanifest.xml)
+    endif()
 endfunction()
