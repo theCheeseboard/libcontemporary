766065692514097a6670be34d618e317ac8312d6
Victor Tran <vicr12345@gmail.com>
2024-03-20 22:36:22 +1100

    Remove stray colon

--- build-project/builder.js
+++ build-project/builder.js
@@ -83,7 +83,7 @@ module.exports = async options => {
             if (process.env["LCNTP_TARGET_PLATFORM"] == "android" && !options.forceHostBuild) {
                 cmakeArgs.push(`-DANDROID_PLATFORM=${process.env["ANDROID_PLATFORM"]}`);
                 cmakeArgs.push(`-DANDROID_ABI=${process.env["ANDROID_ABI"]}`);
-                cmakeArgs.push(`-DCMAKE_FIND_ROOT_PATH:=${process.env["Qt6_DIR"]};${process.env["RUNNER_WORKSPACE"]}/cmake-install/${process.env["ANDROID_ABI"]}`);
+                cmakeArgs.push(`-DCMAKE_FIND_ROOT_PATH=${process.env["Qt6_DIR"]};${process.env["RUNNER_WORKSPACE"]}/cmake-install/${process.env["ANDROID_ABI"]}`);
                 cmakeArgs.push(`-DCMAKE_INSTALL_PREFIX=${process.env["RUNNER_WORKSPACE"]}/cmake-install/${process.env["ANDROID_ABI"]}`);
                 cmakeArgs.push(`-DCNTP_TOOL_PATH=${process.env["RUNNER_WORKSPACE"]}/cmake-install/${options.arch}`);
                 prefixPath = `${process.env["RUNNER_WORKSPACE"]}/cmake-install/${process.env["ANDROID_ABI"]}`
